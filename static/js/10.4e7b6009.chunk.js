a3cd51c30f5832f2960dfc5999bc0a7b6e629744
Sushant Chitrakar <sushantchitrakar@gmail.com>
2020-12-13 13:46:32 +0545

    Build

---
+++ static/js/10.4e7b6009.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpor-admin-dashboard"]=this["webpackJsonpor-admin-dashboard"]||[]).push([[10],{356:function(a,i,n){"use strict";n.r(i);var r=n(1);n(0);i.default=function(){return Object(r.jsx)("div",{children:"Public"})}}}]);
+//# sourceMappingURL=10.4e7b6009.chunk.js.map
